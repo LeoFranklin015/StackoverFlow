bf7d52422a902b48208da3704ad54068c22f9614
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-06-22 13:16:10 +0530

    RightSide Bar completed 😌

--- src/components/HomeMainbar/HomeMainbar.jsx
+++ src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,7 +1,65 @@
 import React from "react";
-
+import "./HomeMainbar.css";
+import { Link, useLocation } from "react-router-dom";
 const HomeMainbar = () => {
-  return <div></div>;
+  var questionsList = [
+    {
+      id: 1,
+      votes: 3,
+      no0fAnswers: 2,
+      questionTitle: "What is a function?",
+      questionBody: "It meant to be",
+      questionTags: ["java", "node js", "react js", "mongoDB"],
+      userPosted: "mano",
+      time: "jan 1",
+    },
+    {
+      id: 2,
+      votes: 0,
+      noofAnswers: 0,
+      questionTitle: "What is a function?",
+      questionBody: "It meant to be",
+      questionTags: ["javascript", "R", "python"],
+      userPosted: "mano",
+      time: "jan 1",
+    },
+    {
+      id: 3,
+      votes: 1,
+      no0fAnswers: 0,
+      questionTitle: "What is a function?",
+      questionBody: "It meant to be",
+      questionTags: ["javascript", "R", "python"],
+      userPosted: "mano",
+      time: "jan 1",
+    },
+  ];
+
+  const location = useLocation();
+  return (
+    <div className="main-bar">
+      <div className="main-bar-header">
+        {location.pathname === "/" ? (
+          <h1>Top Questions</h1>
+        ) : (
+          <h1> All Questions</h1>
+        )}
+
+        <Link to="/AskQuestion" className="ask-btn">
+          Ask Question
+        </Link>
+      </div>
+      <div>
+        {questionsList === null ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <p>{questionsList.length} Questions</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomeMainbar;
--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./RightSidebar.css";
+import Widget from "./Widget";
+import WidgetTags from "./WidgetTags";
 const RightSidebar = () => {
-  return <div></div>;
+  return (
+    <div className="right-sidebar">
+      <Widget />
+      <WidgetTags />
+    </div>
+  );
 };
 
 export default RightSidebar;
