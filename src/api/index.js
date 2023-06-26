fd41fc925c6ccbaf0a93d3435b6d1e4052b2e9b8
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-06-26 18:31:29 +0530

    Hosting chnages

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({
+  baseURL: "https://stackoverflow-fo3t.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
