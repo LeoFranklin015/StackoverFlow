575d9b9c875931e568a911a8ac8569ce9d101145
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-06-28 19:54:10 +0530

    subscription updates

--- src/pages/Subscribe/Payment.jsx
+++ src/pages/Subscribe/Payment.jsx
@@ -1,72 +1,3 @@
-// import {
-//   CardElement,
-//   PaymentElement,
-//   PaymentRequestButtonElement,
-//   useElements,
-//   useStripe,
-// } from "@stripe/react-stripe-js";
-// import React, { useState } from "react";
-// import { updatePayment } from "../../actions/users";
-
-// function PaymentForm() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const stripe = useStripe();
-//   const elements = useElements();
-
-//   const createSubscription = async () => {
-//     try {
-//       const paymentMethod = await stripe.createPaymentMethod({
-//         card: elements.getElement("card"),
-//         type: "card",
-//       });
-//       const response = await fetch("http://localhost:4000/user/payment", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           name,
-//           email,
-//           paymentMethod: paymentMethod.paymentMethod.id,
-//         }),
-//       });
-//       if (!response.ok) return alert("Payment unsuccessful!");
-//       const data = await response.json();
-//       const confirm = await stripe.confirmCardPayment(data.clientSecret);
-//       if (confirm.error) return alert("Payment unsuccessful!");
-//       alert("Payment Successful! Subscription active.");
-//     } catch (err) {
-//       console.error(err);
-//       alert("Payment failed! " + err.message);
-//     }
-//   };
-
-//   return (
-//     <div style={{ width: "40%", marginTop: "100px" }}>
-//       Name:{" "}
-//       <input
-//         type="text"
-//         value={name}
-//         onChange={(e) => setName(e.target.value)}
-//       />
-//       <br />
-//       Email:{" "}
-//       <input
-//         type="email"
-//         value={email}
-//         onChange={(e) => setEmail(e.target.value)}
-//       />
-//       <br />
-//       <CardElement />
-//       <br />
-//       <button onClick={createSubscription}>Subscribe - 5 INR</button>
-//     </div>
-//   );
-// }
-
-// export default PaymentForm;
-
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
@@ -97,6 +28,11 @@ function PaymentForm({ productId }) {
       const confirm = await stripe.confirmCardPayment(response.clientSecret);
       if (confirm.error) {
         return alert("Payment unsuccessful!");
+      }
+      if (productId === "prod_O9pjsBVkDvAtrb") {
+        alert("GOLD MEMBERSHIP");
+      } else {
+        alert("PLATINUM MEMBERSHIP");
       }
 
       alert("Payment Successful! Subscription active.");
