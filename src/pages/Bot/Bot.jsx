fae52de06f0920be5bcf51440d9a85cc68cf2097
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-12 19:34:04 +0530

    Bot added

---
+++ src/pages/Bot/Bot.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useRef, useEffect, useState } from "react";
+
+const Bot = () => {
+  const [message, setMessage] = useState("");
+  const [responses, setResponses] = useState([]);
+  const responseContainerRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newResponse = {
+          question: message,
+          answer: data.message.content,
+        };
+        setResponses([...responses, newResponse]);
+      })
+      .catch((error) => console.log(error));
+    setMessage("");
+  };
+
+  // Scroll to the bottom of the response container when new responses are added
+  useEffect(() => {
+    if (responseContainerRef.current) {
+      responseContainerRef.current.scrollTop =
+        responseContainerRef.current.scrollHeight;
+    }
+  }, [responses]);
+
+  return (
+    <div className="App">
+      <div className="response-container" ref={responseContainerRef}>
+        {responses.map((response, index) => (
+          <div key={index} className="response">
+            <div className="question">{response.question}</div>
+            <pre className="answer">{response.answer}</pre>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="input-form">
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type your message..."
+          className="textarea"
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Bot;
--- src/pages/ChatBot/ChatBot.jsx
+++ src/pages/ChatBot/ChatBot.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import Bot from "../Bot/Bot";
 
 const ChatBot = () => {
+  const user = useSelector((state) => state.authReducer.authData);
+  console.log(user);
   return (
     <div>
       <LeftSidebar />
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
-      <h1>ChatBot.</h1>
+      {!user.result.verified ? (
+        <Bot />
+      ) : (
+        <>
+          <h1>no</h1>
+          <h1>no</h1>
+          <h1>no</h1>
+          <h1>no</h1>
+        </>
+      )}
     </div>
   );
 };
